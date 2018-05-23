6a1554c05a13f1c672693f7b8f9e22fbfa954fef
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ VB/DXExample.Module.Web/WebReportsController.vb
@@ -0,0 +1,25 @@
+Imports Microsoft.VisualBasic
+Imports System
+
+Imports DevExpress.ExpressApp
+
+Namespace DXExample.Module.Web
+	Public Class WebReportsController
+		Inherits ViewController
+		Public Sub New()
+			TargetViewType = ViewType.DetailView
+			TargetObjectType = GetType(MyReportData)
+		End Sub
+		Protected Overrides Overloads Sub OnActivated()
+			MyBase.OnActivated()
+			AddHandler (CType(View.CurrentObject, MyReportData)).ReportCreated, AddressOf WinReportsController_ReportCreated
+		End Sub
+		Private Sub WinReportsController_ReportCreated(ByVal sender As Object, ByVal e As ReportCreatedEventArgs)
+			' Place your code here
+		End Sub
+		Protected Overrides Overloads Sub OnDeactivating()
+			MyBase.OnDeactivating()
+			RemoveHandler (CType(View.CurrentObject, MyReportData)).ReportCreated, AddressOf WinReportsController_ReportCreated
+		End Sub
+	End Class
+End Namespace
