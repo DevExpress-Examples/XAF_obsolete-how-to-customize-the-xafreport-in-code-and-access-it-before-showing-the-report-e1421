49000d3c19ae998896f47bec7bc9d7db6ecdcb77
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/DXExample.Module.Web/WebReportsController.cs
@@ -0,0 +1,23 @@
+using System;
+
+using DevExpress.ExpressApp;
+
+namespace DXExample.Module.Web {
+    public class WebReportsController : ViewController {
+        public WebReportsController() {
+            TargetViewType = ViewType.DetailView;
+            TargetObjectType = typeof(MyReportData);
+        }
+        protected override void OnActivated() {
+            base.OnActivated();
+            ((MyReportData)View.CurrentObject).ReportCreated += new EventHandler<ReportCreatedEventArgs>(WinReportsController_ReportCreated);
+        }
+        void WinReportsController_ReportCreated(object sender, ReportCreatedEventArgs e) {
+            // Place your code here
+        }
+        protected override void OnDeactivated() {
+            base.OnDeactivated();
+            ((MyReportData)View.CurrentObject).ReportCreated -= new EventHandler<ReportCreatedEventArgs>(WinReportsController_ReportCreated);
+        }
+    }
+}
